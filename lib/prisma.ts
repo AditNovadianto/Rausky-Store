5f0ebf773d4b06b92c7e7e08fb1c826f5b537918
boedegoat <bhremada.fka@gmail.com>
2022-05-13 17:34:25 +0700

    add prisma client setup

---
+++ lib/prisma.ts
@@ -0,0 +1,20 @@
+import { PrismaClient } from '@prisma/client'
+
+declare global {
+  // allow global `var` declarations
+  // eslint-disable-next-line no-var
+  var prisma: PrismaClient | undefined
+}
+
+const prisma =
+  global.prisma ||
+  new PrismaClient({
+    log: ['query'],
+  })
+
+if (process.env.NODE_ENV !== 'production') global.prisma = prisma
+
+export default prisma
+
+// TODO: create apiHandler wrapper
+// TODO: setup next-auth (email password, google)
