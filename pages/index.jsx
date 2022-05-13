08002a8ef71f5ef3756815e44d45ef7b81bacbf6
AditNovadianto <aditya.novadianto@gmail.com>
2022-05-13 16:32:17 +0700

    edit navbar section

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,7 +4,7 @@ import Wrapper from '../components/Wrapper'
 const Home = () => {
   return (
     <Container>
-      <Wrapper>Umang ni bos</Wrapper>
+      <Wrapper></Wrapper>
     </Container>
   )
 }
