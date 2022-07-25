ac4bab31a8e999b75bd9f41b53a925471b53e45d
boedegoat <bhremada.fka@gmail.com>
2022-07-25 20:17:39 +0700

    change meta img

--- components/Container.tsx
+++ components/Container.tsx
@@ -32,29 +32,23 @@ const Container: FC<Props> = ({ title, children, noNavbar, noTopMargin }) => {
 
         {/* Open Graph / Facebook */}
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://metatags.io/" />
+        <meta property="og:url" content="https://rausky.vercel.app/" />
         <meta property="og:title" content="Rausky Store" />
         <meta
           property="og:description"
           content="#1 Online Topup store. We provide the best experience to fulfill your entertainment needs."
         />
-        <meta
-          property="og:image"
-          content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
-        />
+        <meta property="og:image" content="/images/logo/overview.jpg" />
 
         {/* Twitter */}
         <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://metatags.io/" />
+        <meta property="twitter:url" content="https://rausky.vercel.app/" />
         <meta property="twitter:title" content="Rausky Store" />
         <meta
           property="twitter:description"
           content="#1 Online Topup store. We provide the best experience to fulfill your entertainment needs."
         />
-        <meta
-          property="twitter:image"
-          content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
-        />
+        <meta property="twitter:image" content="/images/logo/overview.jpg" />
       </Head>
 
       {!noNavbar && <Navbar />}
